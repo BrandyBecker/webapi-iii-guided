ceb1e079412e78443c965ab69d558154734227d8
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2019-10-30 13:03:00 -0400

    + custom urlLogger

---
+++ NOTES.md
@@ -0,0 +1,22 @@
+# Middleware Notes
+--------------------------------------------------------------------------------------------------------
+## Jargon
+Seperation of Concerns.
+_we do NOT write code for the computer, code is  acommunication device, a way to reveal our intention to the next developer_
+
+**EVERYTHING IS MIDDLEWARE**
+Well, almost everything :-)
+
+--------------------------------------------------------------------------------------------------------
+## Types ( based on how we got it or who built it )
+- _built in middleware_: included w/ express. ex: `express.json()`
+- _third party middleware_: must be installed from `npm`
+- _custom middleware_: we code these!!
+
+## Types ( based on how it's being used )
+- global: rune on every request that comes into the server
+- 
+--------------------------------------------------------------------------------------------------------
+Order matters, it goes TOP to BOTTOM and LEFT to RIGHT .
+
+--------------------------------------------------------------------------------------------------------
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet'); // third-party middleware
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
+//----------------------------------------------------------------------------------
+//the three amigos 🟩⬜🟥
+function dateLogger(req,res,next){
+  console.log(new Date().toISOString());
+  next();
+}
+//----------------------------------------------------------------------------------
+//write a middleware function that logs the  HTTP method and the URL visited by the client
+//should log to the console something that looks like this: GET / or GET /api/hubs
+function urlLogger(req, res, next) {
+  console.log(`${req.method}, ${req.url}`);
+  next();
+}
 
-server.use(express.json());
 
+//global middleware - helmet!
+server.use(helmet()); // third-party middleware
+server.use(express.json()); // built-in middleware
+server.use(dateLogger);//custom-built software
+server.use(urlLogger); //custom-built software
+//----------------------------------------------------------------------------------
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
