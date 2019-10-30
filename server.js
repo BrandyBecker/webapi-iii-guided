8c8f1d8f82efc948803eb3119d66cae12cb47f2d
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2019-10-30 13:33:44 -0400

    gatekeeper password w/ or w/o empty string + status codes + messages for errors

--- server.js
+++ server.js
@@ -1,28 +1,72 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet'); // third-party middleware
+const morgan = require('morgan'); //third-party middleware
+
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 //----------------------------------------------------------------------------------
 //the three amigos 🟩⬜🟥
+//## CUSTOM DATE LOGGER MIDDLEWARE
 function dateLogger(req,res,next){
   console.log(new Date().toISOString());
   next();
 }
 //-----------------------------------------------------------------------------------
+//##CUSTOM URLLOGGER MIDDLEWARE
 //write a middleware function that logs the  HTTP method and the URL visited by the client
 //should log to the console something that looks like this: GET / or GET /api/hubs
-function urlLogger(req, res, next) {
-  console.log(`${req.method}, ${req.url}`);
-  next();
+// function urlLogger(req, res, next) {
+//   console.log(`[${new Date().toISOString()}] ${req.method}, ${req.url}`);
+//   next();
+// }
+//-----------------------------------------------------------------------------------
+//CUSTOM PASSWORD=MEOW HEADER MIDDLEWARE
+// NEED password to enter, password = meow 
+// function gateKeeper(req,res,next){
+//   //data can come in the body, url parameters,  query string, headers
+//   //new way of reading data sent by client
+//   const password = req.headers.password || '';
+
+//   if(password.toLowerCase() === 'meow'){
+//     next();
+//   } else {
+//     res.status(400).json({you: 'cannot pass!!'})
+//   }
+// }
+//--------------------------------------------------------------------------------------
+// change the gatekeeper to return a 400 if no password is provided and a message
+// that says please provide a password
+// if a password is provided and it is mellon, call next, otherwise return a 401
+// and the you shall not pass message
+
+function gateKeeper(req,res,next){
+  const password = req.headers.password
+
+  password 
+  ? 
+  password.toLowerCase() === 'mellon' 
+  ? 
+  next() 
+  : 
+  res.status(400).json({you: 'cannot pass!!'}) 
+  : 
+  res.status(401).json({message: 'you need to enter a password!!'})
+
 }
 
 
-//global middleware - helmet!
-server.use(helmet()); // third-party middleware
+
+//-------------------------------------------------------------------------------------
+
+//## GLOBAL MIDDLEWARE =============================================================
+server.use(gateKeeper);
+server.use(helmet()); // third-party security middleware
+server.use(morgan('dev')); //third-party method/url/time/status logger middleware
+
 server.use(express.json()); // built-in middleware
-server.use(dateLogger);//custom-built software
-server.use(urlLogger); //custom-built software
+server.use(dateLogger);//custom-built date logger software
+// server.use(urlLogger); //custom-built method/url logger software
 //----------------------------------------------------------------------------------
 server.use('/api/hubs', hubsRouter);
 
